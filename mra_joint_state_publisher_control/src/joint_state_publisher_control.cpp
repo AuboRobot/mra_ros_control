6409f350c7b105a1371cf2998a4ebb8942f9e21f
lmn <mengnanli@aliyun.com>
2017-05-02 12:01:35 +0800

    add the package mra_joint_state_publisher_control

---
+++ mra_joint_state_publisher_control/src/joint_state_publisher_control.cpp
@@ -0,0 +1,40 @@
+#include "ros/ros.h"
+#include "std_msgs/Float32MultiArray.h"
+#include <moveit/move_group_interface/move_group.h>
+#include <mra_basic/config.h>
+#include <mra_core_msgs/JointCommand.h>
+
+
+mra_core_msgs::JointCommand joints;
+
+ros::Publisher *ptr_pub;
+ros::Rate *ptr_loop_rate;
+
+void chatterCallback(const sensor_msgs::JointState::ConstPtr& msg)
+{
+      for(std::size_t i=0; i < mra_basic_config::jointID.size(); ++i)
+      {
+        joints.command[i] = msg->position[i];
+      }
+      ptr_pub->publish(joints);
+   
+}
+
+
+int main(int argc, char **argv)
+{
+ 
+  ros::init(argc, argv, "joint_state_publisher_control"); 
+  ros::NodeHandle n;
+
+  ros::Publisher command_pub = n.advertise<mra_core_msgs::JointCommand> (JOINT_COMMAND_TOPIC, 1000);
+  ptr_pub = &command_pub;
+  joints.command.resize(mra_basic_config::jointID.size());
+
+ 
+  ros::Subscriber sub = n.subscribe("joint_states", 1000, chatterCallback);
+
+  ros::spin();
+
+  return 0;
+}
